945f65b453725063b52d631468de3f82a8237ea3
Luttarios-069 <117871011+Luttarios-069@users.noreply.github.com>
2023-01-11 11:06:36 -0300

    11/01/2023

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 59;
+
+if(vel > 60){
+    console.log("Você ultrapassou o limite maximo de velocidade.MULTADO!");
+}else {
+    console.log("Velocidade permitida.PARABÉNS!");
+}
+
+console.log("Dirija sempre usando cinto de segurança!");
